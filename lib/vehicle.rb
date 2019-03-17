59b1c61726067bdcf0c22ee451d17ee9afa0af4c
Riccardo <riccardo_london@live.it>
2019-03-17 10:06:02 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,17 +1,8 @@
-class Vehicle
-  attr_accessor :wheel_number, :wheel_size
-
-  def initialize(wheel_size, wheel_number = 4)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
-
+class Vehicle < Car
+  
   def go
     return "vrrrrrrrooom!"
   end
 
-def fill_up_tank
-  return "filling up!"
-  end
 
 end
