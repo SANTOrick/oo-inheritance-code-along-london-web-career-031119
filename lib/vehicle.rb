8149f6fcdb6430b15728990c6420cb55028d8bb9
Riccardo <riccardo_london@live.it>
2019-03-17 10:05:16 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,6 +5,13 @@ class Vehicle
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
-  
+
+  def go
+    return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+def fill_up_tank
+  return "filling up!"
+  end
 
 end
